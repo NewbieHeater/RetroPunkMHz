280f38e2091a55addd886f319ac32e30d7a92989
NewbieHeater <103471494+NewbieHeater@users.noreply.github.com>
2025-05-10 19:47:47 +0900

    review

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -10,39 +10,43 @@ public class PlayerMove : MonoBehaviour
     public int Period;
     public int Waveform;
     public GameObject[] weapon;
-    float hAxis;
-    float vAxis;
-    float chargeTime = 0;
-    float chargeminTime = 1f;
-    bool wDown;
-    bool jDown;
-    int count = 0;
-    bool isJump;
-    bool fDown;
-    bool fUp;
-    bool isCharging;
-    Vector3 moveVec;
 
-    Rigidbody rigid;
-    Animator anim;
+    //접근 한정자 붙여두기(함수에도)
+    //최대한 private를 쓰자!!!
+    private float hAxis;
+    private float vAxis;
+    private float chargeTime = 0;
+    private float chargeminTime = 1f;
+    private bool wDown;
+    private bool jDown;
+    private int count = 0;
+    private bool isJump;
+    private bool fDown;
+    private bool fUp;
+    private bool isCharging;
+    private Vector3 moveVec;
 
-    GameObject nearObject;
-    weapon equipWeapon;
-    void Awake()
-    {
-        
-    }
+    private Rigidbody rigid;
+    private Animator anim;
+
+    private GameObject nearObject;
+
+    //이거는 public이 아니여도 인스펙터창에서 바꿀수있게(다른 스크립트에서는 못바꾸지만 인스펙터에서는 바뀌게)
+    [SerializeField]
+    private weapon equipWeapon;
+
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+
+        //Find사용을 줄이자 일단 이거 하나정도는 괜찮습니다.
         equipWeapon = GameObject.Find("hand").GetComponent<weapon>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        //좋아요 이거 캡슐화
         GetInput();
         Move();
         Turn();
@@ -53,32 +57,47 @@ public class PlayerMove : MonoBehaviour
     
     void GetInput()
     {
+        //이름 바꿔줘요 hAxis제외하고
         hAxis = Input.GetAxisRaw("Horizontal");
+        //z축 안써요 
         vAxis = Input.GetAxisRaw("Vertical");
+
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
         fDown = Input.GetButtonDown("Fire1");
+        //mouseDown
         fUp = Input.GetButtonUp("Fire1");
     }
-
+    private float onDashing = 0.3f;
     void Move()
     {
-        moveVec = new Vector3(hAxis, 0, vAxis).normalized; // y축을 안씀
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized; // z축을 안씀 
+        //찾아보기
+        //rigid.AddForce(moveVec, ForceMode.Impulse);
 
-        transform.position += moveVec * speed * 0.3f * Time.deltaTime;
+        //rigid.velocity = moveVec;
+
+        transform.position += moveVec * speed * onDashing * Time.deltaTime;
+
+        //위 3가지 방법중 적당한 방법 찾아쓰세요
+
         if (wDown)
         {
             transform.position += moveVec * speed * Time.deltaTime;
         }
         else
         {
-            transform.position += moveVec * speed * 0.3f * Time.deltaTime;
+            transform.position += moveVec * speed * onDashing * Time.deltaTime;
         }
+
         anim.SetBool("isRun", wDown);
+
+        //조건문안에 넣어주세요 그냥 써도 상관 없는데 가독성
         anim.SetBool("isWalk", moveVec != Vector3.zero);
     }
     void Turn()
     {
+        //LookAt말고 직접 마우스쪽으로 각도 돌려주기
         transform.LookAt(transform.position + moveVec);
     }
 
@@ -86,7 +105,7 @@ public class PlayerMove : MonoBehaviour
     {
         if (jDown && count < 2)
         {
-
+            //AddForce쓰기
             rigid.velocity = new Vector3(0, 15, 0); 
             anim.SetBool("isJump", true);
             anim.SetTrigger("doJump");
@@ -95,6 +114,7 @@ public class PlayerMove : MonoBehaviour
         
 
     }
+    private float maxChargeTime;
     void Attack()
     {
         if(fDown)
@@ -110,9 +130,9 @@ public class PlayerMove : MonoBehaviour
         {
             if(chargeTime >= chargeminTime)
             {
-                if(chargeTime >5f)
+                if(chargeTime > maxChargeTime)
                 {
-                    chargeTime = 5f;
+                    chargeTime = maxChargeTime;
                     isCharging = false;
                     equipWeapon.ChargeSwing(chargeTime);
                     
@@ -141,4 +161,6 @@ public class PlayerMove : MonoBehaviour
             anim.SetBool("isJump", false);
         }
     }
+
+    //Raycast나 콜라이더를 하나 추가해보세요
 }
