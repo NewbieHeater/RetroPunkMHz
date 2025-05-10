889fea52bf439dce90655ca91cf9df1764fe646c
HardCake4360 <chriskim4360@gmail.com>
2025-05-10 19:00:01 +0900

    테스트 커밋

---
+++ Assets/scripts/puchTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puchTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //asdfhjaslkdjflkasjhd;fjk;asdjkf;lkasdjf;lkjasd;lkfj;laskjdf;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
