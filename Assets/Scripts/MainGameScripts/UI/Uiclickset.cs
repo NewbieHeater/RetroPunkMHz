b73c27705375cdbffcbe958ff615db4b50106a59
son12321-ma <sonmaru12@naver.com>
2025-10-11 23:44:13 +0900

    오브젝트 설치 구현

---
+++ Assets/Scripts/MainGameScripts/UI/Uiclickset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Uiclickset : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private EtoRoominstall _placementManager;
+    private float _lastclick;
+    private float _doubleclicktime = 0.3f;
+
+
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Time.deltaTime - _lastclick < _doubleclicktime)
+        {
+            _placementManager.startPlacing(_gameObject);
+
+        }
+        _lastclick = Time.time;
+    }
+
+}
+
+    
